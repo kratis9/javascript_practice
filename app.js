c9d7d7fb1fb699591c29e6e45911341abe65e31e
krati <kratis9@gmail.com>
2018-05-11 11:56:06 +1000

    create respository

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 var name = 'krati';
 console.log(name);
 
+//change the value of name
+name = 'pihu';
+console.log(name);
+
+
+//Init var
+var greeting;
+console.log(greeting);
+greeting = 'helloo'
+console.log(greeting);
+
+
