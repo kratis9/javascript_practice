c422bd681c042f9aad32f664c787fb3d3ee87fff
krati <kratis9@gmail.com>
2018-05-11 12:33:52 +1000

    describe how to write var names and the let

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-//var,let, const
+// //var,let, const
 
-var name = 'krati';
-console.log(name);
+// var name = 'krati';
+// console.log(name);
 
-//change the value of name
-name = 'pihu';
-console.log(name);
+// //change the value of name
+// name = 'pihu';
+// console.log(name);
 
 
-//Init var
-var greeting;
+// //Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'helloo'
+// console.log(greeting);
+
+// //variable can start only letters, numbers, _, $
+// // can not start with number
+
+
+// //Multi word vars
+// var firstName = 'john'; // Camel Case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'pihi'; // Pascal Case
+
+
+//LET
+let greeting;
 console.log(greeting);
 greeting = 'helloo'
 console.log(greeting);
-
-
