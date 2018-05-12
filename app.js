07a7c077a4eecfd8241c6cfea3c22bfaedbdc523
krati <kratis9@gmail.com>
2018-05-12 13:51:27 +1000

    finis type converstion

--- app.js
+++ app.js
@@ -118,6 +118,14 @@
    val = (5).toString();
    val = (true).toString();
 
+// String to Number
+   val = Number('5');
+   val = Number('true');
+   val = Number('false');
+   val = Number('null');
+   val = Number('hello');
+   val = Number([1,2,3]);
+
 
 
 
@@ -126,4 +134,5 @@
   //Output
     console.log(val);
     console.log(typeof val);
-    console.log(val.length);
+   // console.log(val.length);
+    console.log(val.toFixed());
